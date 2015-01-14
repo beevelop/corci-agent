f086cba4c0375632fc6a25ac22319f693cf45150
Maik Hummel <yo@beevelop.com>
2015-01-14 16:26:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+[![Build Status](https://travis-ci.org/beevelop/corci-agent.svg?branch=master)](https://travis-ci.org/beevelop/corci-agent)
+[![Dependency Status](https://gemnasium.com/beevelop/corci-agent.svg)](https://gemnasium.com/beevelop/corci-agent)
+[![Code Climate](https://codeclimate.com/github/beevelop/corci-agent/badges/gpa.svg)](https://codeclimate.com/github/beevelop/corci-agent)
+
+# corCI-agent
+
+Library for all corCI-agents
+
+# Disclaimer
+
+This project is currently under heavy development and might be unstable. Don't use it in production (unless you're adventurous).
---
+++ index.js
@@ -0,0 +1,59 @@
+var Agent = require('./lib/Agent');
+
+var Common = require('corci-libs').Common;
+var yargs = Common.yargs;
+
+var patch = require('corci-libs').patch;
+// patch on to support binding with multiple events at once
+patch(process.EventEmitter.prototype, ["on", "addListener"]);
+
+function Cmd(platformName, platformClass) {
+
+    var conf = yargs
+        .help('help')
+        .version('0.0.1', 'v')
+        .alias('v', 'version')
+        .showHelpOnFail(true)
+        .usage('Connects the corCI-agent to the corCI-master.\nUsage: $0')
+        .options('p', {
+            alias: 'port',
+            default: 8000,
+            describe: 'Port the server should use'
+        })
+        .options('q', {
+            alias: 'protocol',
+            default: 'http',
+            describe: 'Protocol the server should use (https requires key and cert argument)'
+        })
+        .options('h', {
+            alias: 'host',
+            default: 'localhost',
+            describe: 'Hostname the server should use'
+        })
+        .options('k', {
+            alias: 'keep',
+            default: 0,
+            describe: 'Amount of builds to keep (0 = unlimited)'
+        })
+        .options('l', {
+            alias: 'location',
+            default: 'builds',
+            describe: 'Path to the builds directory'
+        })
+        .options('n', {
+            alias: 'name',
+            describe: 'Name of this agent'
+        })
+        .options('reuseworkfolder', {
+            boolean: true,
+            default: false,
+            describe: 'if enabled empties location for each build'
+        })
+        .argv;
+
+    conf.agent = platformName;
+    var agent = new Agent(config, platformClass);
+    agent.connect();
+}
+
+module.exports = Cmd;
