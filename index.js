bee6188cb379755795b2b251c8b7644b9afe9847
Maik Hummel <yo@beevelop.com>
2015-02-17 00:10:28 +0100

    add Logger (winston) support

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var Agent = require('./lib/Agent');
 
-var Common = require('corci-libs').Common;
+var libs = require('corci-libs');
+var Common = libs.Common;
+var Logger = libs.Logger;
 var yargs = Common.yargs;
 
-var patch = require('corci-libs').patch;
-// patch on to support binding with multiple events at once
-patch(process.EventEmitter.prototype, ["on", "addListener"]);
+Logger.addLevels({
+    agent: 3,
+    build: 2,
+    bupro: 2
+}, {
+    agent: 'blue',
+    build: 'yellow',
+    bupro: 'green'
+});
 
-function Cmd(platformName, platformClass) {
+function Cmd(Captain) {
 
     var conf = yargs
         .help('help')
-        .version('0.0.1', 'v')
+        .version('0.1.0', 'v')
         .alias('v', 'version')
         .showHelpOnFail(true)
         .usage('Connects the CorCI-agent to the corCI-master.\nUsage: $0')
@@ -30,11 +38,6 @@ function Cmd(platformName, platformClass) {
             default: 'localhost',
             describe: 'the server\'s hostname'
         })
-        .options('k', {
-            alias: 'keep',
-            default: 0,
-            describe: 'Amount of builds in location to keep (0 = unlimited)'
-        })
         .options('l', {
             alias: 'location',
             default: 'builds',
@@ -44,17 +47,15 @@ function Cmd(platformName, platformClass) {
             alias: 'name',
             describe: 'this agent\'s name'
         })
-        .options('reuseworkfolder', {
-            boolean: true,
-            default: false,
-            describe: 'if enabled empties location for each build'
-        })
         .argv;
 
-    conf.agent = platformName;
-    conf.url = '{0}://{1}{2}'.format(conf.protocol, conf.host, conf.port === 80 ? '' : ':' + conf.port);
-    var agent = new Agent(conf, platformClass);
-    agent.connect();
+    conf.url = '{0}://{1}{2}/{3}'.format(
+        conf.protocol,
+        conf.host,
+        conf.port === 80 ? '' : ':' + conf.port,
+        'agent'
+    );
+    var agent = new Agent(conf, Captain);
 }
 
 module.exports = Cmd;
